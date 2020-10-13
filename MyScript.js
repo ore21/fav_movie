cd24ff1bee308cf801e4108f0cb2cc7bd606ce9a
ore21 <35785030+ore21@users.noreply.github.com>
2020-10-13 12:47:55 -0500

    favorite movie

---
+++ MyScript.js
@@ -0,0 +1,10 @@
+(function () {
+    var favMovie = ["Godzilla", "Intensity", "Action", "Epicness"];
+    
+    function display(movie)
+    {
+        alert("MyScript.js: " + movie);
+    }
+
+    display(favMovie);
+  })();
